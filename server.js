1a06675568a801ce3fe7337439eb130c5761ccf3
kmarks2013 <kmarks2013@gmail.com>
2020-08-16 22:29:10 -0400

    adds createShopifyAuth logic

--- server.js
+++ server.js
@@ -19,6 +19,20 @@ app.prepare().then(() => {
     const server = new Koa()
     server.use(session( {secure:true, sameSite: 'none' }, server))
     server.keys = [SHOPIFY_API_SECRET_KEY]
+    server.use(
+        createShopifyAuth({
+            apikey: SHOPIFY_API_KEY,
+            secret: SHOPIFY_API_SECRET_KEY,
+            scopes: ['read_products'],
+            afterAuth(ctx) {
+                const { shop, accessToken } = ctx.session
+                ctx.redirect('/')
+            }
+        })
+    )
+
+    server.use(verifyRequest())
+
     server.use(async (ctx) => {
         await handle(ctx.req, ctx.res)
         ctx.respond = false
