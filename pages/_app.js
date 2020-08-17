aadfa20a29865e207b1ac2fedc7026183907c8c2
kmarks2013 <kmarks2013@gmail.com>
2020-08-16 23:45:37 -0400

    adds app render for next.js

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import App from 'next/app'
+import Head from 'next/head'
+
+class MyApp extends App {
+    render () {
+        const { Component, pageProps } = this.props
+        return (
+            <React.Fragment>
+                <Head>
+                    <title>Sample App</title>
+                    <meta charSet='utf-8'/>
+                </Head>
+                <Component {...pageProps} />
+            </React.Fragment>
+        )
+    }
+}
