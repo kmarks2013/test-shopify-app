d7d2119a14f9a70acb6ef75d19e99ed3fba3f47d
kmarks2013 <kmarks2013@gmail.com>
2020-08-16 22:19:46 -0400

    removes whitespace and adds app to sever file" ;

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ const session = require('koa-session')
 dotenv.config()
 
 const port = parseInt(process.env.PORT, 10) || 3000
-const  dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle =  app.getRequestHandler()
 
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env
+
+app.prepare().then(() => {
+
+})
