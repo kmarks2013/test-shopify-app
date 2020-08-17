639e952325023f898db9834e51f68e5e7b5ba30c
kmarks2013 <kmarks2013@gmail.com>
2020-08-16 23:22:38 -0400

    copies in working sample code

--- server.js
+++ server.js
@@ -1,47 +1,47 @@
-require('isomorphic-fetch')
-const dotenv = require('dotenv')
-const Koa = require('koa')
-const next = require('next')
-const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth')
-const { verifyRequest } = require('@shopify/koa-shopify-auth')
-const session = require('koa-session')
+require('isomorphic-fetch');
+const dotenv = require('dotenv');
+const Koa = require('koa');
+const next = require('next');
+const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
+const { verifyRequest } = require('@shopify/koa-shopify-auth');
+const session = require('koa-session');
 
-dotenv.config()
+dotenv.config();
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle =  app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env
+const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
 
 app.prepare().then(() => {
-    const server = new Koa()
-    server.use(session( {secure:true, sameSite: 'none' }, server))
-    server.keys = [SHOPIFY_API_SECRET_KEY]
-    server.use(
-        createShopifyAuth({
-            apikey: SHOPIFY_API_KEY,
-            secret: SHOPIFY_API_SECRET_KEY,
-            scopes: ['read_products'],
-            afterAuth(ctx) {
-                const { shop, accessToken } = ctx.session
-                ctx.redirect('/')
-            }
-        })
-    )
+  const server = new Koa();
+  server.use(session({ secure: true, sameSite: 'none' }, server));
+  server.keys = [SHOPIFY_API_SECRET_KEY];
 
-    server.use(verifyRequest())
+  server.use(
+    createShopifyAuth({
+      apiKey: SHOPIFY_API_KEY,
+      secret: SHOPIFY_API_SECRET_KEY,
+      scopes: ['read_products'],
+      afterAuth(ctx) {
+        const { shop, accessToken } = ctx.session;
 
-    server.use(async (ctx) => {
-        await handle(ctx.req, ctx.res)
-        ctx.respond = false
-        ctx.res.statusCode = 200
-        return
-    })
+        ctx.redirect('/');
+      },
+    }),
+  );
 
-    server.listen(port, () => {
-        console.log(`> Ready on http://localhost:${port}`)
-    })
+  server.use(verifyRequest());
+  server.use(async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+    ctx.res.statusCode = 200;
+    return
+  });
 
-})
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
